af119699c8619dccb912c1f111913452eb476d37
WetDesertRock <WetDesertRock@users.noreply.github.com>
2013-12-14 00:45:28 -0800

    Adding in the history functionality.

--- YouOnlyGotOneLife.py
+++ YouOnlyGotOneLife.py
@@ -11,7 +11,7 @@
 # 
 # *** Secondary Color A:
 # 
-#    var. 1 = #1240AB = rgb(18,64,171)
+#    var. 1 = #1240AB = rgb(18,64,171) #Past player
 #    var. 2 = #2A4480 = rgb(42,68,128)
 #    var. 3 = #06266F = rgb(6,38,111) #Player
 #    var. 4 = #4671D5 = rgb(70,113,213)
@@ -45,18 +45,20 @@ MAT_EMPTY = 0
 COL_SOLID = (0,99,99)
 COL_EMPTY = (0,153,153)
 COL_PLAYER = (6,38,111)
+COL_PPLAYER = (18,64,171)
 
 class World(object):
-    def __init__(self,gsize,sp):
+    def __init__(self,gsize,sp,mh):
         self.gridsize = gsize #Avaliable sizes: 1, 2, 3, 4, 6, 8, 9, 12, 16, 18, 24, 32, 36, 48, 64, 72, 96, 144, 192, 288
         self.spawnpoint = sp
         self.grid = []
         for i in xrange(576/gsize):
             self.grid.append([0]*(576/gsize))
-        pprint.pprint(self.grid)
+        
+        self.maxhistory = mh
+        self.ticks = 0
     
     def isOpen(self,x,y):
-        print x,y
         if x < 0 or x >= len(self.grid) or y < 0 or y >= len(self.grid):
             return False
             
@@ -73,13 +75,17 @@ class World(object):
                 elif self.grid[x][y] == MAT_EMPTY:
                     col = COL_EMPTY
                 pygame.draw.rect(surf,col,((x*self.gridsize,y*self.gridsize),(self.gridsize,self.gridsize)))
-            
+        
+    def tick(self):
+        self.ticks += 1
 
 class Player(object):
     def __init__(self, world):
         self.world = world
         self.curpos = world.spawnpoint
         self.history = [self.curpos]
+        self.maxhistory = self.world.maxhistory
+        self.isShadow = False
     
     def move(self,d):
         nx,ny = self.curpos
@@ -93,18 +99,53 @@ class Player(object):
         else:
             self.curpos = (nx,ny)
             self.history.append(self.curpos)
+            if not self.isShadow and len(self.history)-1 == self.maxhistory:
+                self.isShadow = True
+                
             return True
     
-    def draw(self,surf):
+    def draw(self,surf,iscurrent):
+        if iscurrent:
+            col = COL_PLAYER
+        else:
+            col = COL_PPLAYER
         cx,cy = self.curpos
         gs = self.world.gridsize
-        pygame.draw.rect(surf,COL_PLAYER,((cx*gs,cy*gs),(gs,gs)))
+        pygame.draw.rect(surf,col,((cx*gs,cy*gs),(gs,gs)))
+    
+    def seek(self, i):
+        i = min(len(self.history)-1,max(0,i)) #Constrain
+        print i
+        self.curpos = self.history[i]
+    
+    def sync(self):
+        if self.isShadow:
+            localtick = world.ticks%self.maxhistory
+            self.seek(localtick)
 
-world = World(32,(2,2))
-plr = Player(world)
+world = World(24,(2,2),3)
+curplr = Player(world)
+players = [curplr]
 
 world.grid[5][4] = MAT_SOLID
-        
+
+def moveCurPlayer(d):
+    global curplr, players, world
+    move_succeeded = curplr.move(d) 
+    if not move_succeeded:
+        return False
+    
+    world.tick()
+    pygame.display.set_caption(str(world.ticks))
+    
+    if curplr.isShadow:
+        curplr = Player(world)
+        players.append(curplr)
+    
+    for plr in players:
+        plr.sync()
+    
+    return True
 
 while running:
 	Clock.tick(60)
@@ -122,16 +163,17 @@ while running:
 			print "Wrote Screenshot_%d.png" % stime
 		
 		elif event.key == pygame.K_UP:
-		    plr.move(D_UP)
+		    moveCurPlayer(D_UP)
 		elif event.key == pygame.K_DOWN:
-		    plr.move(D_DOWN)
+		    moveCurPlayer(D_DOWN)
 		elif event.key == pygame.K_RIGHT:
-		    plr.move(D_RIGHT)
+		    moveCurPlayer(D_RIGHT)
 		elif event.key == pygame.K_LEFT:
-		    plr.move(D_LEFT)
+		    moveCurPlayer(D_LEFT)
 	
 	screen.fill((0,0,0))
 	world.draw(screen)
-	plr.draw(screen)
+	for plr in players:
+		plr.draw(screen,plr is curplr)
 	
 	pygame.display.update()
