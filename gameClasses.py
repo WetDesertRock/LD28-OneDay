397161d9e23a722d9e0e2ef5c2a880405fed1841
WetDesertRock <WetDesertRock@users.noreply.github.com>
2013-12-19 17:48:47 -0800

    Licensing. GPLv3 for code, CC BY for sound. Individual licenses for the fonts.

--- gameClasses.py
+++ gameClasses.py
@@ -1,3 +1,20 @@
+#Copyright 2013 WetDesertRock
+#
+#     This file is part of Before Night Falls.
+# 
+#     Before Night Falls is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     Before Night Falls is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with Before Night Falls.  If not, see <http://www.gnu.org/licenses/>.
+
 import pygame
 import pygame.gfxdraw
 from consts import *
